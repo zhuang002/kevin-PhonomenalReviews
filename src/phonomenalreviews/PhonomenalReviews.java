dae6f8a040f5cf19241503b4ae8880e9bb052633
zhuang002 <zhuang001@gmail.com>
2020-09-12 15:14:32 -0400

    basic

--- src/phonomenalreviews/PhonomenalReviews.java
+++ src/phonomenalreviews/PhonomenalReviews.java
@@ -5,32 +5,96 @@
  */
 package phonomenalreviews;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author zhuan
  */
 public class PhonomenalReviews {
 
-    static int pathLength=0;
-    static int n=0;
+    static int pathLength = 0;
+    static int[][] map = null;
+    static int[] phoRestaurants = null;
+    static int[] steps = null;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         readInput();
-        int node=0;
-        int start=findFurthest(0);
-        int end=findFurthest(start);
-        System.out.println(2*(n-1-pathLength)+pathLength);
-    
-}
+        preprocess();
+        int start = findFurthest(phoRestaurants[0]);
+        int end = findFurthest(start);
+        System.out.println(2 * (map.length - 1 - pathLength) + pathLength);
+    }
 
     private static void readInput() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        map = new int[n][n];
+        phoRestaurants = new int[n];
+        steps = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            phoRestaurants[i] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            int pho = sc.nextInt();
+            phoRestaurants[pho] = 1;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            map[x][y] = 1;
+            map[y][x] = 1;
+        }
     }
 
     private static int findFurthest(int node) {
+        ArrayList<Integer> current = new ArrayList();
+        ArrayList<Integer> next = new ArrayList();
+        for (int i = 0; i < steps.length; i++) {
+            steps[i] = -1;
+        }
+        steps[node] = 0;
+        current.add(node);
+        int step = 1;
+        while (!current.isEmpty()) {
+            for (Integer n : current) {
+                for (int j = 0; j < map.length; j++) {
+                    if (map[n][j] == 1 && steps[j] == -1) {
+                        next.add(j);
+                        steps[j] = step;
+                    }
+                }
+            }
+            current = next;
+            next = new ArrayList();
+            step++;
+        }
+        pathLength = -1;
+        int retNode = -1;
+        for (int i = 0; i < map.length; i++) {
+            if (phoRestaurants[i] == 1) {
+                if (pathLength < steps[i]) {
+                    pathLength = steps[i];
+                    retNode = i;
+                }
+            }
+        }
+        return retNode;
+    }
+
+    private static void preprocess() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
